784de920580b77d41df6f48a4bea6216feb047d6
Frazer Seymour <frazer@seymourandco.com>
2014-09-26 23:56:49 -0400

    Added everything from Google Drive.

---
+++ helper.js
@@ -0,0 +1,50 @@
+/**
+ * Creates a new audio graph object based on the function specified.
+ * Creates a button to play the audio graph.
+ *
+ * @param	expression	the function to base the audio graph on as a string
+ * @param	duration	length of the audio graph playback in seconds
+ * @param	div			div to create the button on
+ */
+function addAudioGraph(expression, duration, div) {
+	var audioGraph = new AudioGraph(expression, function(){document.getElementById(expression).style.display = "block";});
+	var container = document.getElementById(div);
+	
+	var button = document.createElement("h3");
+	button.innerHTML = "Play";
+	button.id = expression;
+	button.className = "PlayButton";
+	button.onclick = function() { // Note this is a function
+		audioGraph.play(duration);
+	};
+	writeStyle();
+
+	container.appendChild(button);
+}
+
+
+
+/**
+ * Writes <style> tags to the document.
+ */
+function writeStyle() {	
+	var text = 
+        ["<style>",
+		 "h3.PlayButton {",
+		     "display: none;",
+		     "font-size: 1.17em;",
+			 "background-color: grey;",
+			 "color: white;",
+			 "border: solid 2px black;",
+			 "border-radius: 5px;",
+			 "cursor: pointer;",
+			 "padding-left: 0.4em;",
+			 "width: 2.5em;",
+         "}",
+		 "h3.PlayButton:hover {",
+		     "background-color: white;",
+			 "color: grey;",
+         "}",
+		 "</style>"].join('\n');
+	document.write(text);
+}
---
+++ metrics.js
@@ -0,0 +1,117 @@
+/**
+ * Temporarily stores session data.
+ */
+var data;
+
+
+
+
+/*******************
+ * Cookie Handling *
+ ******************/
+
+/**
+ * Creates a cookie called 'SonificationMetricData' containing the information
+ * stored in 'data'.
+ */
+function bakeCookie() {
+  var cookie = ["SonificationMetricData=", JSON.stringify(data), "; domain=.", window.location.host.toString(), "; path=/;"].join('');
+  document.cookie = cookie;
+}
+
+
+/**
+ * Deletes the 'SonificationMetricData' cookie.
+ *
+ * (Currently unused, keeping for future need/reference.)
+ */
+function deleteCookie() {
+  document.cookie = ["SonificationMetricData=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain=.", window.location.host.toString()].join('');
+}
+
+
+/**
+ * Inserts a string representation of the 'data' object into an element at a
+ * specified id and disables said element to prevent tampering by the user.
+ *
+ * (Called when submit page is loaded.)
+ *
+ * @param	id	ID of the element in which to submit the cookie
+ */
+function submitCookie(id) {
+  var element = document.getElementById(id);
+  element.value = JSON.stringify(data);
+  //element.disabled = true;
+  //element.style.display = "none";
+  element.disabled = false;
+  element.style.display = "block";
+}
+
+
+/**
+ * Initializes 'data' with the contents of the 'SonificationMetricData' cookie
+ * if it exists or an empty array if it does not.
+ *
+ * (Called each time a graph is loaded.)
+ */
+function readCookie() {
+ var result = document.cookie.match(new RegExp("SonificationMetricData=([^;]+)"));
+ if (result && (result = JSON.parse(result[1])))
+ {
+  data = result;
+ }
+ else
+ {
+  data = {"sessions":[]};
+ }
+}
+
+
+
+
+/*********************
+ * Question Handling *
+ ********************/
+
+/**
+ * Initializes an object representing the user's interaction with the current
+ * audio graph and appends it to 'data'.
+ *
+ * (Called each time a graph is loaded.)
+ *
+ * @param	name	Name of the function currently being represented (i.e. "f1").
+ */
+function initQuestion(name) {
+  var date = new Date();
+
+  session = {
+	"graph"		: name,				// Name of graph (i.e., "f1")
+	"startTime"	: date.getTime(),	// Time the user loaded the page (in milliseconds since 01/01/1970)
+	"replays"	: 0					// Number of times the sound is played
+  };
+
+  data.sessions.push(session);
+}
+
+
+/**
+ * Updates the object representing the user's interaction with the current audio
+ * graph.
+ *
+ * (Called each time a graph is played.)
+ */
+function updateQuestion() {
+  var date = new Date();
+  var num = data.sessions.length - 1;	// Current graph is most recent in array.
+
+  // Update replay count.
+  data.sessions[num].replays = data.sessions[num].replays + 1;
+
+  // Stores the last time the user played the graph (same format as 'startTime').
+  data.sessions[num].endTime = date.getTime();
+
+  // Determines and stores the total time the user interacted with the graph (in milliseconds).
+  data.sessions[num].totalTime = data.sessions[num].endTime - data.sessions[num].startTime;
+
+  bakeCookie();
+}
